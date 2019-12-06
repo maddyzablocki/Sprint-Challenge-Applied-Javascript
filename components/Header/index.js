08e6f314757f38e73c0158dda7352004c34dcc7b
maddyzablocki <zablockimadison@gmail.com>
2019-12-06 11:47:35 -0500

    second commit, header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,29 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+function Header(data) {
+        const headerComponent = document.querySelector('.header-container');
+        const header = document.createElement('div');
+        const date = document.createElement('span');
+        const h1 = document.createElement('h1');
+        const temp = document.createElement('span');
 
-function Header() {}
+
+    header.appendChild(date);
+    header.appendChild(h1);
+    header.appendChild(temp);
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = "SMARCH 28, 2019";
+    h1.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+    headerComponent.appendChild(header);
+
+    return data;
+}
+
+window.onload = Header;
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,21 @@
 // Step 2: Create Tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
-// Once the data is returned console.log it and review the structure.
+// [x]Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+// [x] Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then(response => {
+    console.log(response);
+})
+
+function Tabs (data) {
+    const tabComponents = document.querySelector(.topics);
+    const tab = document.createElement('div');
+
+
+}
